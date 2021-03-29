a8db91e6a54fcab13e2bbc56872f066484e3416d
Jackie Moraa <jackiemoraa@Jackies-MacBook.local>
2021-03-29 21:07:17 +0300

    Problem 58: Length of Last Word #Easy

--- lengthOfLastWord.py
+++ lengthOfLastWord.py
@@ -19,7 +19,20 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         result = s.split(" ")
         n = len(result)
-        for i in range(n-1, -1, -1):
+        for i in range(n - 1, -1, -1):
             if len(result[i]) != 0:
                 return len(result[i])
         return 0
+
+    # Came across this solution that eliminates the need for the loop in my first implementation.
+    # It basically strips all the trailing spaces so we needn't worry about handling an input like "a b    "
+    def lengthOfLastWord2(self, s: str) -> int:
+        s = s.strip().split(" ")
+        if len(s) == 0:
+            return 0
+        else:
+            return len(s[-1])
+
+
+
+
