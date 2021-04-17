22f97034cf0217651c92e5a159595b7c590bae2b
Jackie Moraa <jackiemoraa@Jackies-MacBook.local>
2021-04-17 14:30:53 +0300

    Problem 27. Remove Element. #Easy

---
+++ removeElement.py
@@ -0,0 +1,19 @@
+"""
+Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1)
+extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+"""
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        n = len(nums)
+        for i in range(n):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count += 1
+        return count
